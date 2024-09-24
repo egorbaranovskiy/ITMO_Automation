5052fcdab05a6991d99f42194f213a58c26c5589
egorbaranovskiy/2_hw <ebaranovskij17@gmail.com>
2024-09-24 21:47:42 +0300

    First Commit

---
+++ home_work/3_hw.py
@@ -0,0 +1,13 @@
+a = 5
+b = 50
+if a > b:
+    print(a)
+else:
+    print(b)
+
+
+a, b = int (input()), int (input())
+if a - b == 135:
+    print('yes')
+else:
+    print('no')
---
+++ python_trening /task_8_if_else.py
@@ -0,0 +1,13 @@
+num = -5
+if num >= 0:
+    print ('Число больше либо равно 0')
+else:
+    print('Число отрицательно')
+
+a = 5
+if a > 100:
+    print('-')
+elif a < -100:
+    print('-')
+else:
+    print('+')
